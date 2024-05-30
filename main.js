c64adf905d7a901e251064deadb876457d4b81c4
Bekhruz20 <163981178+Bekhruz20@users.noreply.github.com>
2024-05-30 15:03:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+alert(`Sizda random sonlar chiqishni boshlaydi`)
+
+let random_num = Math.floor(Math.random() * 100)
+
+alert(`Random son: ${random_num}  `)
+
+alert(`Biz random soni 2 ga kopaytiramiz va 2 ga bolamiz`)
+
+let a=2
+let b=3
+alert(`Randomoni son ${random_num} edi
+uni 2 kopaytirsa ${random_num*a}
+uni 2 bolsa ${random_num/a}
+uni 3 ga bolganda ${random_num%b} qoldiq chiqadi
+`)
+
+
+
+
+// let preDicrease=10
+// let preIncrease=15
+
+// preIncrease++
+// preDicrease--
+
+// console.log(preIncrease)
+// console.log(preDicrease)
+
+// let b=10
+
+// // b=b+5
+// b+=5
+
+// console.log(b)
